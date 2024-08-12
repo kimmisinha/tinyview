ead3399f925871c8df0fcaf419c2db054e97db5f
kimmisinha <kimmi.sinha@newput.com>
2024-08-12 16:49:11 +0530

    Display CustomCardList only below md breakpoint

--- src/components/HomeComponent/Homecomponent.jsx
+++ src/components/HomeComponent/Homecomponent.jsx
@@ -3,14 +3,24 @@ import CustomCardList from "../CardComponents/CustomCardList";
 import UserComponets from "../UserComponents/UserComponets";
 import Navbar from "../Navbar/Navbar";
 import AllSeriesComponent from "../AllSeriesComponent/AllSeriesComponent";
+import { useTheme, useMediaQuery } from "@mui/material";
 
 function Homecomponent() {
+  const theme = useTheme();
+  const showCustomCardList = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <div style={{ display: "flex" }}>
-      <Navbar />
-      <UserComponets />
-      <CustomCardList />
-      <AllSeriesComponent />
+      {showCustomCardList ? (
+        <CustomCardList />
+      ) : (
+        <>
+          <Navbar />
+
+          <UserComponets />
+          <CustomCardList />
+          <AllSeriesComponent />
+        </>
+      )}
     </div>
   );
 }
